a23a3e98db477d82a9399ecccfdb967aa6a622d8
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-08-06 12:46:08 +0530

    adds sample

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping("/rest")
+class DemoController{
+
+	@Autowired
+	Environment environment;
+
+	@GetMapping("/hello")
+	public String sayHello() throws UnknownHostException {
+		return "Hello Good morning from "+ InetAddress.getLocalHost().getHostAddress();
+	}
+}
